59b0455b0d74979e4a52dd6630e0e305df8ff817
deepak179-s <deepak17943@gmail.com>
2026-09-22 16:09:35 +0530

    fix: whitelist supabase storage domain in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "ghchart.rshah.org",
       },
+      {
+        protocol: "https",
+        hostname: "irunidgtivonmksjvddr.supabase.co",
+      }
     ],
   },
 };
